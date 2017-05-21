e0cb4ca5ab56097ada515111b687e027594d5e02
Justin Bland <jrbland@yahoo.com>
2017-05-21 00:26:41 +0000

    added permission to lesson show page

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
+
   def show
   end
   
@@ -8,4 +11,15 @@ class LessonsController < ApplicationController
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+  
+  
+  private
+  
+  def require_authorized_for_current_course
+    @current_course = current_lesson.section.course
+    unless current_user.enrolled_in?(@current_course)
+      redirect_to course_path(@current_course), alert: 'You are not currently enrolled.  Please enroll to see lesson.'
+    end
+  end
+
 end
